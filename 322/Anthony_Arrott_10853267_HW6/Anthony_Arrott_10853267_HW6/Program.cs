9781f45a98f7cf7362cf6f75788febeae1221439
ajarrott <mrajarrott@gmail.com>
2014-10-16 21:31:44 -0700

    Homework 6

---
+++ 322/Anthony_Arrott_10853267_HW6/Anthony_Arrott_10853267_HW6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anthony_Arrott_10853267_HW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression x = new Expression();
+            x.menu();
+        }
+    }
+}
